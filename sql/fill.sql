67435b051e2db0312e15a951db5c2f317964ced3
dshehbaj <shehbaj.dhillon@gmail.com>
2021-04-18 17:45:34 -0700

    Added sql sourcing file.

---
+++ sql/fill.sql
@@ -0,0 +1,13 @@
+source ./tables.sql;
+
+source ./states_fill.sql;
+source ./counties_fill.sql;
+
+source ./state_life_expec.sql;
+source ./county_life_expec.sql;
+
+source ./state_realty_fill.sql;
+source ./counties_realty_fill.sql;
+
+source ./state_covid_vacc.sql;
+
